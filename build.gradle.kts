5b09855abf53be4af7e908395bb01aad4627581f
Joshua Prince <joshua@jtprince.com>
2025-07-27 23:20:04 -0700

    Bump packetevents dependency to 2.9.5-SNAPSHOT for 1.21.8
    Remove sonatype repo (no longer needed for Adventure)

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,6 @@ val javaVersion: JavaLanguageVersion = JavaLanguageVersion.of(17)
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
-    maven("https://s01.oss.sonatype.org/content/repositories/snapshots") // Adventure (required by PacketEvents)
     maven("https://repo.codemc.org/repository/maven-public/") // PacketEvents
 }
 
@@ -34,7 +33,7 @@ dependencies {
     // Compile Only dependencies: Neither shaded nor needed by API consumers (assumed they'll already add it themselves)
     compileOnly("io.papermc.paper:paper-api:$paperApiVersion")
     // Shade and Relocate: Shaded into plugin, exposed to API consumers with relocated names if necessary
-    implementation("com.github.retrooper:packetevents-spigot:2.9.0-SNAPSHOT")
+    implementation("com.github.retrooper:packetevents-spigot:2.9.5-SNAPSHOT")
     implementation("org.bstats:bstats-bukkit:3.0.2")
     implementation("com.jeff-media:MorePersistentDataTypes:2.4.0")
 
